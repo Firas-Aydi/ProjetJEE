2774a814c546905c49cc130d08c8ab063684b01c
Firas-Aydi <firasaydi0@gmail.com>
2020-12-11 23:55:12 +0100

    Create Form_Ajouter.js

---
+++ Form_Ajouter.js
@@ -0,0 +1,10 @@
+
+    $(document).ready(function () {
+        $('#f').hide();
+        $('#m').click(function () {
+            $('#f').hide();
+        });
+        $('#p').click(function () {
+            $('#f').show();
+        });
+    });
